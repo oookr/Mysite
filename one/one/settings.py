ff0c99c4f60da361975febe31b5728c6a3239053
oookr <32430440+oookr@users.noreply.github.com>
2017-12-29 21:57:59 +0300

    oops

--- one/one/settings.py
+++ one/one/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-
+SECRET_KEY = ' o3 + uodskm (-7)) kiqt1idnxzsr $ 3! 9% &% 8 = 3 $ n76s + p (6_ ^ h *) '
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

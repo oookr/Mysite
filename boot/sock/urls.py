af6ddc24396cfb1a24116e4be70f54bc7153ad94
oookr <32430440+oookr@users.noreply.github.com>
2018-01-02 01:20:46 +0300

    django with html links

--- boot/sock/urls.py
+++ boot/sock/urls.py
@@ -1,4 +1,4 @@
-"""one URL Configuration
+"""sock URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.0/topics/http/urls/
@@ -13,9 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.urls import include, path
 from django.contrib import admin
-from django.urls import path
+
 
 urlpatterns = [
+	path('home/', include('home.urls')),
+	path('about_us/', include('about_us.urls')),
     path('admin/', admin.site.urls),
 ]
--- boot/sock/wsgi.py
+++ boot/sock/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for one project.
+WSGI config for sock project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "one.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sock.settings")
 
 application = get_wsgi_application()

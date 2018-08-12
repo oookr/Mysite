af26c60bb2576690b6799396e4d2ea5ddc87141a
oookr <32430440+oookr@users.noreply.github.com>
2018-08-12 23:54:08 +0300

    The page 'contact /' is connected (work with Python in Html)

--- mainApp/views.py
+++ mainApp/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
 	return render(request, 'mainApp/homePage.html')
+
+def contact(request):
+	return render(request, 'mainApp/basic.html',{'valuse':['Если у вас остались вопросы, то задавайте их мне по телефону', '+1(676)1234']})

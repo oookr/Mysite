5519ff31f89e3191606fa1f4a50f3d5bf2781be9
oookr <32430440+oookr@users.noreply.github.com>
2018-08-18 19:48:06 +0300

    Upgrade mainApp/views.py

--- mainApp/views.py
+++ mainApp/views.py
@@ -4,4 +4,4 @@ def index(request):
 	return render(request, 'mainApp/homePage.html')
 
 def contact(request):
-	return render(request, 'mainApp/basic.html',{'valuse':['Если у вас остались вопросы, то задавайте их мне по телефону', '+1(676)1234']})
+	return render(request, 'mainApp/basic.html',{'valuse':['Если у вас остались вопросы, то задавайте их мне по телефону', '+1 (676) 1234']})

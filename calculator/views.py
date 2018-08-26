3e57cf77d390544b2e9d217632b94cb871f1e72e
oookr <32430440+oookr@users.noreply.github.com>
2018-08-26 03:11:12 +0300

    Update calculator/views.py

--- calculator/views.py
+++ calculator/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 def index(request):
 	return render(request, 'mainApp/calculator.html')
 def kcah(request):
-	return render(request,)
+	return render(request, 'mainApp/dark_wrapper.html')
